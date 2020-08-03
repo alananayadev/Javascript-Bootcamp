24e54411eaecc717b986d670dccccbaf0ffdf8f7
Alan Anaya Araujo <anaya.araujo@icloud.com>
2020-08-03 18:53:09 -0500

    Async & Sync section

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,11 @@
+//HTTP (Hypertext Transfer Protocol)
+//Request - What do we want to do
+//Response - What was actually done
+
 const puzzlEl = document.querySelector('#hangman-word')
 const guessesEl = document.querySelector('#guesses')
-
 const newGame = new Hangman("nueva Jersey", 4)
+
 puzzlEl.textContent = newGame.puzzle
 guessesEl.textContent = newGame.statusMessages
 
@@ -15,3 +19,29 @@ window.addEventListener('keypress', e => {
         guessesEl.textContent = newGame.statusMessages
     }
 })
+
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+})
+
+
+// const countryCode = 'MX'
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', e => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const result = data.find(country => country.alpha2Code === countryCode)
+//         console.log(result ? result.name : 'Sorry we couldn\'t find your country')
+//     } else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data')
+//     }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
+
