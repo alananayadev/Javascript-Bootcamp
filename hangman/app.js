1857af620a0727ca755fa79d4dd5ad4c29c9056d
Alan Anaya <a.anayaraujo@gmail.com>
2020-08-05 16:44:42 -0500

    Callback challenge

--- hangman/app.js
+++ hangman/app.js
@@ -28,20 +28,11 @@ getPuzzle((error, puzzle) => {
     }
 })
 
-
-// const countryCode = 'MX'
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener('readystatechange', e => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText)
-//         const result = data.find(country => country.alpha2Code === countryCode)
-//         console.log(result ? result.name : 'Sorry we couldn\'t find your country')
-//     } else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data')
-//     }
-// })
-
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
-// countryRequest.send()
+getCountry('MX', (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(`Country name: ${country.name}`)
+    }
+})
 
--- hangman/requests.js
+++ hangman/requests.js
@@ -13,3 +13,21 @@ const getPuzzle = (callback) => {
     request.open('GET','http://puzzle.mead.io/puzzle')
     request.send()
 }
+
+const getCountry = (countryCode, callback) => {
+    const countryRequest = new XMLHttpRequest()
+
+    countryRequest.addEventListener('readystatechange', e => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            const result = data.find(country => country.alpha2Code === countryCode)
+            callback(undefined, result)
+            
+        } else if (e.target.readyState === 4) {
+            callback('Unable to fetch data', undefined)
+        }
+    })
+
+    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+    countryRequest.send() 
+}
