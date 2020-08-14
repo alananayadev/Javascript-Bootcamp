c1e7616375be2f9a587ccf061771bf631975053c
Alan Anaya Araujo <anaya.araujo@icloud.com>
2020-08-13 20:16:25 -0500

    Added closures example

---
+++ Functions/closures.js
@@ -0,0 +1,39 @@
+const createCounter = () => {
+    let count = 0
+
+    return {
+        increment() {
+            count++
+        },
+        decrement() {
+            count --
+        }, 
+        get() {
+            return count
+        }
+    }
+}
+
+const counter = createCounter()
+counter.increment()
+counter.decrement()
+counter.decrement()
+console.log(counter.get())
+
+// Adder
+const createAdder = (a) => {
+    return (b = 2) => {
+        return a + b
+    }
+}
+
+const add10 = createAdder(10)
+console.log(add10(4))
+console.log(add10(20))
+
+const createTipper = (tip = 15) => {
+    return bill => bill * (tip/100)
+}
+
+const tip = createTipper()
+console.log(tip(100))
